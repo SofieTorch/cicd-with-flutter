691fd392f49c4f1642a48a47a6226b3d5f18d879
Sofia Toro <sofie.torch@outlook.com>
2022-04-23 10:51:23 -0400

    implement initial ui

---
+++ lib/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            '''
+This app only checks for internet connectivity, but hey!
+It implements a CI/CD workflow with GitHub Actions ;)
+            ''',
+          ),
+        ),
+      ),
+    );
+  }
+}
